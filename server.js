b61cb93743a4ddadcdc9037d8fe75d18b1c4118f
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2021-11-14 19:23:54 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,19 +12,51 @@ app.use(cors());
 
 // Get an ID number that hasn't already been used in albums
 function newID() {
-    // Get list of IDs
-    let ids = albumsData.map(el => el.albumId).sort();
-    let nextId = 1;
-    // check if id string is taken
-    while(ids.includes(`${nextId}`)) {
-        nextId++;
-    }
-    return nextId;
+  // Get list of IDs
+  let ids = albumsData.map((el) => el.albumId).sort();
+  let nextId = 1;
+  // check if id string is taken
+  while (ids.includes(`${nextId}`)) {
+    nextId++;
+  }
+  return String(nextId);
 }
 
 app.get("/albums", (request, response) => {
-    response.status(200).send(albumsData);
+  response.status(200).send(albumsData);
 });
-
-
+app.get("/albums/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const idAlbum = albumsData[id];
+  res.status(200).send(idAlbum);
+});
+app.post("/albums", (req, res) => {
+    console.log(req.body);
+    const newObj = {
+      albumId: newID(),
+      artistName: req.body.artistName,
+      collectionName: req.body.collectionName,
+      artworkUrl100: req.body.artworkUrl100,
+      releaseDate: req.body.releaseDate,
+      primaryGenreName: req.body.primaryGenreName,
+      url: req.body.url,
+    }; 
+    console.log(newObj)
+    albumsData.push(newObj)
+    res.sendStatus(201).send("Created")
+})
+app.delete("/albums/:id", (req, res) => {
+  // find album and make sure it's there => use findIndex method to get the index
+  // if not there (index === -1) => 404 not found
+  // if is there => use splice(index, 1) to remove the element at index
+  // response.sendStatus(204)
+});
+app.put("/albums/:id", (req, res) => {
+  // find album (and make sure it's there) => use findIndex method to get the index
+  // if not there (index === -1) => 404 not found
+  // updatedAlbum = req.body
+  // (optionally) check that the albumId of updatedAlbum is correct
+  // albumsData[index] = updatedAlbum
+  // reponse.sendStatus(204)
+});
 app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
